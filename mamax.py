4619ab7cbe7e5377ebf1fb2f9b8bca2dac5796d2
vu <vuduytuananh@gmail.com>
2018-03-22 15:31:55 -0400

    show 2 at the time

--- mamax.py
+++ mamax.py
@@ -8,23 +8,25 @@ train_images_dir = os.getcwd() + "/input/train_images/"
 image_dir_name = "images"
 mask_dir_name = "masks"
 count = 0
-def show_time(file_path):
-    pic = mpimg.imread(file_path)
-    print(file_path)
-    plt.imshow(pic)
+def show_time(pic_data):
+    plt.imshow(pic_data)
     plt.show()
+
 def process_images(image_path):
     print("Processing image...")
-    show_time(image_path)
+
 def process_mask(mask_path):
     print("Processing mask...")
-    show_time(mask_path)
 
 for sub_dir in os.listdir(train_images_dir):
+    pic_data = numpy.array([])
     for sd in os.listdir(train_images_dir + sub_dir):
         dir_path = train_images_dir + sub_dir + "/" + sd + "/"
         file_path = dir_path + os.listdir(dir_path)[0]
         # image
-        if sd == image_dir_name: process_images(file_path)
+        if sd == image_dir_name:
+            pic_data = mpimg.readimg(file_path)
         # mask
-        else: process_mask(file_path)
+        else:
+            pic_data += mpimg.readimg(file_path)
+            show_time(pic_data)
